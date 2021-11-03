82322b787cda2b83c253cd88e470c664bc4e3da6
padalasurendramac <53860717+padalasurendramac@users.noreply.github.com>
2021-11-03 19:03:26 +0530

    Create main.tf

---
+++ aws/EC2/security_group/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "us-west-2"   # us-west-2 oregan region and below image from same region available ami id
+}
+
+variable "server_port" {
+  description = "web_server"
+  default = 8080
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example"
+  ingress {
+    from_port = "${var.server_port}"
+    to_port = "${var.server_port}"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-0b28dfc7adc325ef4"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+}
+
+
